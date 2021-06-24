7438a9ac45aa23b8cb43ed26fbeda16521750009
NattakanSp <nattagan.sanpa@gmail.com>
2021-06-24 22:26:29 +0700

    Submit homework week 1

---
+++ WK1/JavascriptHomework.js
@@ -0,0 +1,156 @@
+1.Comment Your JavaScript Code
+//natakan//
+/*natakan*/
+2.Declare JavaScript Variables
+var myName;
+3.Storing Values with the Assignment Operator
+// Setup
+var a;
+
+// Only change code below this line
+a=7;
+4.Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b=a;
+5.var a=9;
+6.// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a=5;
+b=10;
+c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+7.// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+8.var sum = 10 + 0;
+sum = sum + 10;
+9.var difference = 45 - 0;
+difference = difference - 32;
+10.var difference = 45 - 33;
+11.var product = 8 * 10;
+12.var quotient = 66 / 33;
+13.var myVar = 87;
+
+// Only change code below this line
+++myVar;
+14.var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 2.5;
+15.var product = 2.0 * 2.5;
+16.var quotient = 4.4 / 2.0; // Change this line
+17.// Only change code below this line
+
+var remainder;
+remainder= 11%3;
+18.var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+19.var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+20.var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+21.var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /=  12;
+b /=  4;
+c /=  11;
+22.var myStr = "I am a \"double quoted\" string inside \"double quotes\"."
+23.var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+24.var myStr="FirstLine\n\t\\SecondLine\nThirdLine";// Change this line
+25.var myStr = "This is the start." + " This is the end."; // Change this line
+26.// Only change code below this line
+
+var myStr = "This is the first sentence."
+myStr += " This is the second sentence.";
+27.// Only change code below this line
+var myName="Nattakan";
+var myStr = "Hello, Are you ok?" + myName + "Hope you get what you wish";
+28.// Change code below this line
+
+var someAdjective = "challenging";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+29.
+var myFisrtName = " Nattakan";
+var myLastName = "Sanpa";
+30.var myStr = '<a href="http://www.example.com" target="_blank">Link</a>' ;
+31.// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+console.log("lastNameLength".length);
+
+32.// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+var firstLetterOfLastName = lastName;
+firstLetterOfLastName = lastName[0]; // Change this line
+33.// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World";
+34.// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+35.// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
+36.// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+37.var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
